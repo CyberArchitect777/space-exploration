059a793a115b88d922f5791e1a9f24e6c5ae6ea2
“CyberArchitect777” <169656116+CyberArchitect777@users.noreply.github.com>
2024-11-30 19:25:48 +0000

    feat: place window in a container and background in main area only

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        "starbg": "url('https://res.cloudinary.com/dp1ehadna/image/upload/v1732987858/aperture-vintage-Z6EpCdMcoUU-unsplash_lf4uon.jpg')",
+        "starbg1": "url('https://res.cloudinary.com/dp1ehadna/image/upload/v1732987858/aperture-vintage-Z6EpCdMcoUU-unsplash_lf4uon.jpg')",
+        "starbg2": "url('https://res.cloudinary.com/dp1ehadna/image/upload/v1732992313/ba9czsotpmj0hay873fi.jpg')",
+        "starbg3": "url('https://res.cloudinary.com/dp1ehadna/image/upload/v1732992313/c1qlgorinpeysxbogplt.jpg')",
+        "starbg4": "url('https://res.cloudinary.com/dp1ehadna/image/upload/v1732992313/qmjzt66z8zbwzvdid6ee.jpg')",
+        "starbg5": "url('https://res.cloudinary.com/dp1ehadna/image/upload/v1732992314/ufp8xr5c66wcehfufnw5.jpg')",
+        "starbg6": "url('https://res.cloudinary.com/dp1ehadna/image/upload/v1732992315/k3bcqtldouvbkxw2kp2k.jpg')",
       }
     },
   },
